7e19c40390988570ccb8f32fb14627755073849f
Le Viet Bach <bleviet@gmail.com>
2026-01-31 14:56:50 +0100

    chore: The root templates folder was empty and not used by the application, so I deleted it.

--- build.py
+++ build.py
@@ -18,7 +18,7 @@ def main():
     
     add_data = [
         ("app/assets", "app/assets"),
-        ("templates", "templates"),
+        ("app/resources/templates", "app/resources/templates"),
     ]
     
     # Construct PyInstaller arguments
